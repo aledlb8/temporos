d308b3b4ce687b099d0f485d0a34fe0b9bb9f03b
aledlb8 <74106092+aledlb8@users.noreply.github.com>
2023-04-20 20:59:31 -0400

    Improved error handling and added cluster support

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 const scheduler = require('./lib/scheduler');
 const concurrency = require('./lib/concurrency');
-const Task = require('./lib/task.js')
+const Task = require('./lib/task.js');
+const ClusterScheduler = require('./lib/cluster-scheduler.js');
+
+process.on('unhandledRejection', (err) => {
+    console.error('Unhandled error:', err);
+});
 
 module.exports = {
-    Task,
-    cancelTask: scheduler.cancelTask.bind(scheduler),
-    scheduleTask: scheduler.scheduleTask.bind(scheduler),
-    getRunningTasks: scheduler.getRunningTasks.bind(scheduler),
-    setMaxConcurrency: concurrency.setMaxConcurrency.bind(concurrency),
-    startConcurrencyMonitor: concurrency.start.bind(concurrency),
-    stopConcurrencyMonitor: concurrency.stop.bind(concurrency)
+  Task,
+  Scheduler: scheduler,
+  ClusterScheduler,
+  cancelTask: scheduler.cancelTask.bind(scheduler),
+  scheduleTask: scheduler.scheduleTask.bind(scheduler),
+  getRunningTasks: scheduler.getRunningTasks.bind(scheduler),
+  setMaxConcurrency: concurrency.setMaxConcurrency.bind(concurrency),
+  startConcurrencyMonitor: concurrency.start.bind(concurrency),
+  stopConcurrencyMonitor: concurrency.stop.bind(concurrency)
 };
