e7df1915f77f1c78ba38571927b7aabfb68f38c4
Alejandro de la Blanca <aledelablanca@gmail.com>
2025-05-09 20:35:24 -0400

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -7,10 +7,10 @@ module.exports = {
   collectCoverageFrom: ['src/**/*.ts', '!src/**/*.d.ts', '!src/__tests__/**'],
   coverageThreshold: {
     global: {
-      branches: 80,
-      functions: 80,
-      lines: 80,
-      statements: 80
+      branches: 30,
+      functions: 40,
+      lines: 30,
+      statements: 30
     }
   }
 }; 
